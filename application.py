6836d7bb0bc79773540f0f264bf2b470e3185680
Balabanov Maxim <bma22l120@student-bmstu.ru>
2025-08-24 18:35:14 +0300

    API + testing API

---
+++ application.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import pickle
+
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+    
+class PredictionInput(BaseModel):
+    RevolvingUtilizationOfUnsecuredLines: float
+    age: int
+    DebtRatio: float
+    MonthlyIncome: float
+    NumberOfDependents: float
+    HasPastDue: int
+    HasCreditLoanOrLine: int
+
+app = FastAPI()
+
+@app.get('/health')
+def health():
+    return {'status': 'OK'}
+
+@app.post('/predict')
+def predict(input_data: PredictionInput):
+    data = pd.DataFrame({
+        'RevolvingUtilizationOfUnsecuredLines': [input_data.RevolvingUtilizationOfUnsecuredLines],
+        'age': [input_data.age],
+        'DebtRatio': [input_data.DebtRatio],
+        'MonthlyIncome': [input_data.MonthlyIncome],
+        'NumberOfDependents': [input_data.NumberOfDependents],
+        'HasPastDue': [input_data.HasPastDue],
+        'HasCreditLoanOrLine': [input_data.HasCreditLoanOrLine]
+    })
+    
+    prediction = 'Reject' if model.predict(data) == 1 else 'Accept'
+    
+    return f'{prediction} client\'s request'
+
+if __name__ == '__main__':
+    import uvicorn
+    
+    uvicorn.run(app, host='127.0.0.1', port=5000)
---
+++ test_api.py
@@ -0,0 +1,40 @@
+import pytest
+import requests
+
+URL = 'http://127.0.0.1:5000'
+
+@pytest.fixture(scope='function')
+def resource_setup(request):
+    print('SETUP: Getting test data...')
+    
+    data_test_0 = {
+            'RevolvingUtilizationOfUnsecuredLines': 0.766127,
+            'age': 45,
+            'DebtRatio': 0.802982,
+            'MonthlyIncome': 9120.0,
+            'NumberOfDependents': 2.0,
+            'HasPastDue': 1,
+            'HasCreditLoanOrLine': 1
+    }
+    
+    def resource_teardown():
+        print("\nTEARDOWN")
+        
+    request.addfinalizer(resource_teardown)
+    
+    return data_test_0
+
+
+
+def test_health(resource_setup):
+    response = requests.get(URL + '/health')
+    assert response.json().get('status') == 'OK'
+    
+def test_prediction_1(resource_setup):
+    print('STARTING test_prediction_1...')
+    
+    response = requests.post(URL + '/predict', json=resource_setup)
+    
+    print(response.json())
+    
+    assert response.json() == "Accept client's request"
